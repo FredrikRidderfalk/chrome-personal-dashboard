6346eea6902d099d66b1d4d469210f3f8994e549
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-05 19:27:15 +0100

    updated fonts

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ fetch(
   .then((data) => {
     console.log(data.urls.full);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
+    if (data.description !== null) {
+      document.getElementById("author").textContent = `${data.description}`;
+    }
   })
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
