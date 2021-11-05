69a9a6e2ae9831a7abb2e46a5568ddb8c0f20a3a
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-05 16:41:37 +0100

    added solana crypto data and time

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ fetch(
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/solana")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+          <img src=${data.image.small} />
+          <span>${data.name}</span>
+      `;
+    document.getElementById("crypto-bottom").innerHTML = `
+          <p>🪙 Price: €${data.market_data.current_price.eur}</p>
+          <p>🤑 24H High: €${data.market_data.high_24h.eur}</p>
+          <p>🔻 24H Low: €${data.market_data.low_24h.eur}</p>
+      `;
+  })
+  .catch((err) => console.error(err));
+
+const ticker = setInterval(() => {
+  const time = new Date().toLocaleTimeString("en-GB");
+  document.getElementById("time").textContent = `${time}`;
+}, 10);
